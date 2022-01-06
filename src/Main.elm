daa6bc8dd5c38bced5dddbe9f376d64fda15d818
Ryan1729 <Ryan1729@gmail.com>
2022-01-06 00:06:00 -0700

    add placeholder images

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,34 @@
 module Main exposing (main)
 
-import Html exposing (text)
+import Html exposing (div, img)
+import Html.Attributes exposing (src, style)
+
+
+redPixel =
+    "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQImWO45+n5HwAF6QJwj+ULYQAAAABJRU5ErkJggg=="
+
+
+greenPixel =
+    "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQImWMw2JD3HwAEsAJOci/TgAAAAABJRU5ErkJggg=="
+
+
+bluePixel =
+    "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQImWMwDnr4HwAEiAJm7qe+oQAAAABJRU5ErkJggg=="
+
+
+image source =
+    img
+        [ src source
+        , style "height" "20ex"
+        , style "width" "10em"
+        ]
+        []
 
 
 main =
-    text "Hello!"
+    div
+        []
+        [ image redPixel
+        , image greenPixel
+        , image bluePixel
+        ]
