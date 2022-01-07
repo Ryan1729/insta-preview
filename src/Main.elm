e877149f4957d43012e9ff81f4f2722361bc7f9e
Ryan1729 <Ryan1729@gmail.com>
2022-01-07 00:06:00 -0700

    allow uploading images in each slot, once.

--- src/Main.elm
+++ src/Main.elm
@@ -9,60 +9,6 @@ import Html.Events exposing (onClick)
 import Task
 
 
-redPixel =
-    "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQImWO45+n5HwAF6QJwj+ULYQAAAABJRU5ErkJggg=="
-
-
-greenPixel =
-    "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQImWMw2JD3HwAEsAJOci/TgAAAAABJRU5ErkJggg=="
-
-
-bluePixel =
-    "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQImWMwDnr4HwAEiAJm7qe+oQAAAABJRU5ErkJggg=="
-
-
-profile source =
-    img
-        [ src source
-        , class "profile"
-        , style "width" "30em"
-        , style "height" "20ex"
-        ]
-        []
-
-
-cell source =
-    img
-        [ src source
-        , style "height" "20ex"
-        ]
-        []
-
-
-grid =
-    div
-        [ id "insta-preview-grid"
-        ]
-        [ profile bluePixel
-        , div
-            [ class "cells"
-            ]
-            [ cell redPixel
-            , cell greenPixel
-            , cell bluePixel
-            , cell redPixel
-            , cell greenPixel
-            , cell bluePixel
-            , cell redPixel
-            , cell greenPixel
-            , cell bluePixel
-            , cell redPixel
-            , cell greenPixel
-            , cell bluePixel
-            ]
-        ]
-
-
 
 --
 -- MAIN
@@ -85,14 +31,58 @@ main =
 --
 
 
+type alias ImageState =
+    Maybe ImageSource
+
+
+imageDefault : ImageState
+imageDefault =
+    Nothing
+
+
+
+-- A URL
+
+
+type alias ImageSource =
+    String
+
+
 type alias Model =
-    { image : Maybe String
+    { profile : ImageState
+    , cell0 : ImageState
+    , cell1 : ImageState
+    , cell2 : ImageState
+    , cell3 : ImageState
+    , cell4 : ImageState
+    , cell5 : ImageState
+    , cell6 : ImageState
+    , cell7 : ImageState
+    , cell8 : ImageState
+    , cell9 : ImageState
+    , cell10 : ImageState
+    , cell11 : ImageState
     }
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( Model Nothing, Cmd.none )
+    ( { profile = imageDefault
+      , cell0 = imageDefault
+      , cell1 = imageDefault
+      , cell2 = imageDefault
+      , cell3 = imageDefault
+      , cell4 = imageDefault
+      , cell5 = imageDefault
+      , cell6 = imageDefault
+      , cell7 = imageDefault
+      , cell8 = imageDefault
+      , cell9 = imageDefault
+      , cell10 = imageDefault
+      , cell11 = imageDefault
+      }
+    , Cmd.none
+    )
 
 
 
@@ -101,10 +91,38 @@ init _ =
 --
 
 
+type ImageIndex
+    = Zero
+    | One
+    | Two
+    | Three
+    | Four
+    | Five
+    | Six
+    | Seven
+    | Eight
+    | Nine
+    | Ten
+    | Eleven
+
+
+type ImageLocation
+    = Profile
+    | Cell ImageIndex
+
+
+
+-- TODO `ShiftDown ImageIndex`
+
+
+type ImageAction
+    = Replace ImageLocation
+
+
 type Msg
-    = ImageRequested
-    | ImageSelected File
-    | ImageUrl String
+    = ImageRequested ImageAction
+    | ImageSelected ImageAction File
+    | ImageUrl ImageAction ImageSource
 
 
 
@@ -126,20 +144,65 @@ imageTypes =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        ImageRequested ->
+        ImageRequested action ->
             ( model
-            , Select.file imageTypes ImageSelected
+            , Select.file imageTypes (ImageSelected action)
             )
 
-        ImageSelected file ->
+        ImageSelected action file ->
             ( model
-            , Task.perform ImageUrl (File.toUrl file)
+            , Task.perform (ImageUrl action) (File.toUrl file)
             )
 
-        ImageUrl url ->
-            ( { model | image = Just url }
+        ImageUrl action url ->
+            ( case action of
+                Replace location ->
+                    replace location url model
             , Cmd.none
             )
+
+
+replace : ImageLocation -> ImageSource -> Model -> Model
+replace location source model =
+    case location of
+        Profile ->
+            { model | profile = Just source }
+
+        Cell Zero ->
+            { model | cell0 = Just source }
+
+        Cell One ->
+            { model | cell1 = Just source }
+
+        Cell Two ->
+            { model | cell2 = Just source }
+
+        Cell Three ->
+            { model | cell3 = Just source }
+
+        Cell Four ->
+            { model | cell4 = Just source }
+
+        Cell Five ->
+            { model | cell5 = Just source }
+
+        Cell Six ->
+            { model | cell6 = Just source }
+
+        Cell Seven ->
+            { model | cell7 = Just source }
+
+        Cell Eight ->
+            { model | cell8 = Just source }
+
+        Cell Nine ->
+            { model | cell9 = Just source }
+
+        Cell Ten ->
+            { model | cell10 = Just source }
+
+        Cell Eleven ->
+            { model | cell11 = Just source }
 
 
 
@@ -150,24 +213,94 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ grid
-        , case model.image of
-            Nothing ->
-                button [ onClick ImageRequested ] [ text "upload image" ]
-
-            Just url ->
-                div []
-                    [ img
-                        [ src url
-                        , style
-                            "height"
-                            "20ex"
-                        ]
-                        []
-                    , p [ style "white-space" "pre" ] [ text url ]
-                    ]
+    div
+        [ id "insta-preview-grid"
         ]
+        [ profile model.profile
+        , div
+            [ class "cells"
+            ]
+            [ cell Zero model.cell0
+            , cell One model.cell1
+            , cell Two model.cell2
+            , cell Three model.cell3
+            , cell Four model.cell4
+            , cell Five model.cell5
+            , cell Six model.cell6
+            , cell Seven model.cell7
+            , cell Eight model.cell8
+            , cell Nine model.cell9
+            , cell Ten model.cell10
+            , cell Eleven model.cell11
+            ]
+        ]
+
+
+redPixel =
+    "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQImWO45+n5HwAF6QJwj+ULYQAAAABJRU5ErkJggg=="
+
+
+greenPixel =
+    "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQImWMw2JD3HwAEsAJOci/TgAAAAABJRU5ErkJggg=="
+
+
+bluePixel =
+    "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQImWMwDnr4HwAEiAJm7qe+oQAAAABJRU5ErkJggg=="
+
+
+profileWidth =
+    style "width" "30rem"
+
+
+profileHeight =
+    cellHeight
+
+
+cellWidth =
+    style "width" "10rem"
+
+
+cellHeight =
+    style "height" "10rem"
+
+
+profile imageState =
+    case imageState of
+        Nothing ->
+            button
+                [ profileWidth
+                , profileHeight
+                , onClick (ImageRequested (Replace Profile))
+                ]
+                [ text "upload image" ]
+
+        Just source ->
+            img
+                [ src source
+                , class "profile"
+                , profileWidth
+                , profileHeight
+                ]
+                []
+
+
+cell index imageState =
+    case imageState of
+        Nothing ->
+            button
+                [ cellWidth
+                , cellHeight
+                , onClick (ImageRequested (Replace (Cell index)))
+                ]
+                [ text "upload image" ]
+
+        Just source ->
+            img
+                [ src source
+                , cellWidth
+                , cellHeight
+                ]
+                []
 
 
 
