5fff74694cfa2f7ed13b9d0cc23f107438166463
Ryan1729 <Ryan1729@gmail.com>
2022-01-07 00:06:00 -0700

    add in upload demo based on File.Select example

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,12 @@
 module Main exposing (main)
 
-import Html exposing (div, img)
+import Browser
+import File exposing (File)
+import File.Select as Select
+import Html exposing (Html, button, div, img, p, text)
 import Html.Attributes exposing (class, id, src, style)
+import Html.Events exposing (onClick)
+import Task
 
 
 redPixel =
@@ -34,7 +39,7 @@ cell source =
         []
 
 
-main =
+grid =
     div
         [ id "insta-preview-grid"
         ]
@@ -56,3 +61,121 @@ main =
             , cell bluePixel
             ]
         ]
+
+
+
+--
+-- MAIN
+--
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+
+--
+-- MODEL
+--
+
+
+type alias Model =
+    { image : Maybe String
+    }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( Model Nothing, Cmd.none )
+
+
+
+--
+-- UPDATE
+--
+
+
+type Msg
+    = ImageRequested
+    | ImageSelected File
+    | ImageUrl String
+
+
+
+-- The MIME types for the image types listed at:
+-- https://developer.mozilla.org/en-US/docs/Web/HTML/Element/img#supported_image_formats
+
+
+imageTypes =
+    [ "image/apng"
+    , "image/avif"
+    , "image/gif"
+    , "image/jpeg"
+    , "image/png"
+    , "image/svg+xml"
+    , "image/webp"
+    ]
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        ImageRequested ->
+            ( model
+            , Select.file imageTypes ImageSelected
+            )
+
+        ImageSelected file ->
+            ( model
+            , Task.perform ImageUrl (File.toUrl file)
+            )
+
+        ImageUrl url ->
+            ( { model | image = Just url }
+            , Cmd.none
+            )
+
+
+
+--
+-- VIEW
+--
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ grid
+        , case model.image of
+            Nothing ->
+                button [ onClick ImageRequested ] [ text "upload image" ]
+
+            Just url ->
+                div []
+                    [ img
+                        [ src url
+                        , style
+                            "height"
+                            "20ex"
+                        ]
+                        []
+                    , p [ style "white-space" "pre" ] [ text url ]
+                    ]
+        ]
+
+
+
+--
+-- SUBSCRIPTIONS
+--
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
